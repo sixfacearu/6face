98c3a9b3b544a861998fa056c806389775628c2c
sixfacearu <sixface0324@gmail.com>
2018-06-19 17:19:28 +0530

    file

---
+++ copytext.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class copytext{
+public static void main(String args[])throws IOException{
+FileInputStream in=null;
+FileOutputStream out=null;
+in=new FileInputStream("input.txt");
+out=new FileOutputStream("output.txt");
+int c;
+while((c=in.read())!=-1){
+  out.write(c);
+}
+}
+}
