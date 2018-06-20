d376f5fe1b779967a189af386dc4db7e3800aaaa
sixfacearu <sixface0324@gmail.com>
2018-06-20 16:29:51 +0530

    update

---
+++ pattern.java
@@ -0,0 +1,15 @@
+public class pattern{
+public static void main(String args[]){
+for(int i=0; i<=5;i++){
+for(int j=1;j<=5;j++){
+if(j<=i){
+System.out.print(" ");
+}
+else{System.out.print("*");
+}
+}
+System.out.println(" ");
+}
+}
+}
+
