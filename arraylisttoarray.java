85244f694d0d3454e875789cd91f78c0437959fd
sixfacearu <sixface0324@gmail.com>
2018-06-22 15:29:29 +0530

    collection

---
+++ arraylisttoarray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class arraylisttoarray{
+	public static void main(String[] args) {
+
+	    /* Array Declaration and initialization*/
+	    String array[]={"Hi", "Hello", "Howdy", "Bye"};
+
+	    /*ArrayList declaration*/
+	    ArrayList<String> arraylist= new ArrayList<String>();
+
+	    /*Conversion*/
+	    Collections.addAll(arraylist, array);
+
+	    /*Adding new elements to the converted List*/
+	    arraylist.add("String1");
+	    arraylist.add("String2");
+
+	    /*Display array list*/
+	    for (String str: arraylist)
+	    {
+	 	System.out.println(str);
+	    }
+	}
+}
